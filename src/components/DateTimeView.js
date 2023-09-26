20d6e2a7236270082e5f4c2594357db5f496c339
MuhsinMohamed <muhsin.mohamed@waypoint-systems.com>
2023-09-26 15:16:51 +0400

    DATE CSS

--- src/components/DateTimeView.js
+++ src/components/DateTimeView.js
@@ -1,4 +1,4 @@
-import { Card, CardHeader } from '@mui/material';
+import { Card, CardHeader, Typography } from '@mui/material';
 import React, { useEffect } from 'react';
 
 const DateTimeView = () => {
@@ -11,12 +11,22 @@ const DateTimeView = () => {
         return () => clearInterval(interval);
     }, []);
     return (
-        <Card  sx={{m:2,pb:2}}>
-            <CardHeader
-            title={date.toLocaleTimeString()} 
-            subheader={`${date.toLocaleDateString('en-US', { weekday: 'long' })}, ${date.toLocaleDateString()}`}            
-            /> 
-        </Card> 
+        <Card sx={{ m: 2,p:1 }}>
+            <Typography variant="h4"
+                sx={{ p: 1,pb:0 }}>
+                {date.toLocaleTimeString()}
+            </Typography>
+            <Typography variant="h6"
+                sx={{  p:1,pt:0 }}>
+                {`${date.toLocaleDateString('en-US', { weekday: 'long' })}, ${date.toLocaleDateString()}`}
+            </Typography>
+            {/* <CardHeader
+                sx={{ p: 2, fontSize: 15 }}
+                // title={date.toLocaleTimeString()} 
+                // style={{fontSize:20}}
+                title={`${date.toLocaleDateString('en-US', { weekday: 'long' })}, ${date.toLocaleDateString()}`}
+            /> */}
+        </Card>
     );
 };
 
